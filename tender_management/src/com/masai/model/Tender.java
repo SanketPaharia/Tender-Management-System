dd4dfec22b7ece5b1f1de84e11ccfbf2ec3ebd51
Sanket Paharia <sanket.paharia@gmail.com>
2022-11-13 18:32:03 +0530

    new

--- tender_management/src/com/masai/model/Tender.java
+++ tender_management/src/com/masai/model/Tender.java
@@ -1,5 +1,59 @@
 package com.masai.model;
 
+import java.util.Date;
+
 public class Tender {
 
+	
+	private String id;
+	private String name;
+	private String type;
+	private int price;
+	private String desc;
+	private Date deadline;
+	private String location;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public Date getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
 }
